591ad7fea7a2ab9b429a34805ef5a2e8b3b33a3c
shristi9090 <156301174+shristi9090@users.noreply.github.com>
2024-01-12 12:11:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+
+const num1 = 5;
+const num2 = 10;
+const sum = addNumbers(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
